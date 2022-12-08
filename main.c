1d8606e25b544ed2d04a41bf1ec6d0b728f987aa
The West <45957199+lawalogunsola63@users.noreply.github.com>
2022-12-08 12:09:55 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,9 +3,51 @@
 #include <string.h>
 #include <math.h> 
 
+
 int main()
 {
-    printf("hello");
+    printf(" MARKET CALCULATOR \n");
 
-    return o;
+    float num1, num2, answer;
+    char op;
+
+    printf(" Enter your calculation: ");
+    scanf("%f %c %f", &num1, &op, &num2);
+
+    switch (op)
+    {
+    case '+':
+        answer = num1 + num2;
+        printf("%f", answer);
+        break;
+
+    case '*':
+        answer = num1 * num2;
+        printf("%f", answer);
+        break;
+    
+    case '-':
+        answer = num1 - num2;
+        printf("%f", answer);
+        break;
+
+    case '/':
+        answer = num1 / num2;
+        printf("%f", answer);
+        break;
+
+    case '^':
+        answer = pow(num1,num2); 
+        printf("%f", answer);
+        break;
+
+    default:
+        printf("invalid operator \n");
+        break;
+    }
+
+
+    return 0;
 }
+
+
